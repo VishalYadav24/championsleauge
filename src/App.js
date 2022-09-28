ce002afb68ec0f7cd33a654124fb02cc4b0918fd
VishalYadav24 <vishal.yadav@3pillarglobal.com>
2022-09-28 11:10:23 +0530

    adding routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import "./App.css";
+import NotFound from "./components/404/notfound.component";
+import Dashboard from "./components/Home/dashboard.component";
+import Layout from "./components/Layout/layout.component";
 
-import './App.css';
+const router = createBrowserRouter(
+  createRoutesFromElements(
+  <Route path="/" element={<Layout />} errorElement={<NotFound/>}>
+   <Route index element={<Dashboard/>}></Route>
+  </Route>)
+);
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
