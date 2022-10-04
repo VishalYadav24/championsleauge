16a6809960228f6db6f0a603deb040d2921045cb
VishalYadav24 <vishal.yadav@3pillarglobal.com>
2022-10-04 17:57:27 +0530

    addition - new route and component watch list

--- src/App.js
+++ src/App.js
@@ -9,13 +9,16 @@ import "./App.css";
 import NotFound from "./components/404/notfound.component";
 import Dashboard from "./components/Home/dashboard.component";
 import Layout from "./components/Layout/layout.component";
+import WatchList from "./components/Watchlist/watchlist.component";
 import theme from "./theme/theme";
 import { getListOfChampions } from "./utils/api";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
   <Route path="/" element={<Layout />} errorElement={<NotFound/>}>
-   <Route index element={<Dashboard/>} loader={getListOfChampions}></Route>
+   <Route index element={<Dashboard/>} loader={getListOfChampions}>
+   </Route>
+   <Route path="favorites" element={<WatchList/>}></Route>
   </Route>)
 );
 
