ee5eb607a373e4d713458ffe982f0359324ab1f9
VishalYadav24 <vishal.yadav@3pillarglobal.com>
2022-10-11 13:26:26 +0530

    adding api for additional info

--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -22,15 +22,28 @@ export const getListOfChampions = async () => {
 };
 
 export const getChampion = async (championName) => {
+  console.log(championName)
+  const additionalDataURL = "https://lolbe2.azurewebsites.net/api/v1/champstaticdata";
   const config = {
     params: {
       "search[name]": championName,
       token: API_KEY,
     },
   };
+  const additionalConfig = {
+    params: {
+      "champion": championName,
+      
+    },
+  };
+  
   try {
     const response = await axios.get(`${baseUrl}`, config);
-    return response?.data;
+    const additionalResponse = await axios.get(`${additionalDataURL}`,additionalConfig);
+    const introduction = {"description":additionalResponse.data[0]?.introduction};
+    const finalResponse = [{...response.data[0],...introduction}]
+    console.log(finalResponse)
+    return finalResponse;
   } catch (error) {
     throw error;
   }
