ca72ce0f15842f8d04aa2b8a75f451d7d7af53fd
VishalYadav24 <112162266+VishalYadav24@users.noreply.github.com>
2022-11-02 10:14:15 +0530

    addition page not found component

--- src/components/404/notfound.component.jsx
+++ src/components/404/notfound.component.jsx
@@ -1,9 +1,24 @@
 import React from "react";
 import { useRouteError } from "react-router-dom";
+import "./notfound.styles.scss";
 
 const NotFound = () => {
   const error = useRouteError();
-  return <div>NotFound {error?.message}</div>;
+  console.log(error)
+  return (
+    <div className="not_found_banner">
+      <p className="title">Page Not Found</p>
+      <img
+        className="error_image"
+        src={require("../../assets/vecteezy_404-landing-page_6549647.jpg")}
+        alt="error image"
+      ></img>
+      {error?.status === 404 ? <p> Please correct path </p> : null
+      } 
+       {error?.message === "Network Error" ? <p> Please check your internet connection </p> : <p>{error?.message || error?.statusText}</p>
+      } 
+    </div>
+  );
 };
 
 export default NotFound;
