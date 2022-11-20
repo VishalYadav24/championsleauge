afb5e73dc96e0e2ceb4944567349ce0073ab9542
VishalYadav24 <112162266+VishalYadav24@users.noreply.github.com>
2022-11-20 19:20:02 +0530

    addition : page count change and styles

--- src/components/Home/dashboard.component.jsx
+++ src/components/Home/dashboard.component.jsx
@@ -1,43 +1,72 @@
 import { Box, FormControl, InputLabel, MenuItem, Select } from "@mui/material";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import { getListOfChampions } from "../../utils/api";
 import HerosList from "../Heros List/herosList.component";
 import Navbar from "../Navbar/navbar.component";
-
+import "./dashboard.styles.scss";
 /**
  * Container component for application components
  */
 const Dashboard = () => {
   const [herosList, setHerosList] = useState([]);
+  const [orderBy, setOrderBy] = useState("hp");
   const listOfChampions = useLoaderData();
-  const [heroCount,setHeroCount] = useState(5);
-  const createLimitedStatsList = (herosList)=>{
-  return  herosList?.map((data) => {
-      return { id: data?.id, name: data?.name, image: data?.image_url,hp:data?.hp,armor: data?.armor,attackDamage: data?.attackdamage};
+  const [heroCount, setHeroCount] = useState(listOfChampions?.length);
+  const createLimitedStatsList = (herosList) => {
+    return herosList?.map((data) => {
+      return {
+        id: data?.id,
+        name: data?.name,
+        image: data?.image_url,
+        hp: data?.hp,
+        armor: data?.armor,
+        attackDamage: data?.attackdamage,
+      };
     });
   };
-  const handleCountChange = async (event)=>{
-    if(event){
-    let heroList;
-    setHeroCount(event?.target?.value);
-   await getListOfChampions(event?.target?.value).then(async(data) => await (heroList = data));
-    console.log(heroList)
-    const minimalList = createLimitedStatsList( heroList);
-    setHerosList(minimalList);
+  const handleCountChange = async (event) => {
+    if (event) {
+      let heroList;
+      setHeroCount(event?.target?.value);
+      await getListOfChampions(event?.target?.value).then(
+        async (data) => await (heroList = data)
+      );
+      console.log(heroList);
+      const minimalList = createLimitedStatsList(heroList);
+      setHerosList(minimalList);
     }
-  }
+  };
+
+  useEffect(() => {
+    const temp = herosList;
+    temp.sort((a, b) => {
+      if (a[orderBy] == b[orderBy]) {
+        return 0;
+      }
+      return a[orderBy] < b[orderBy] ? -1 : 1;
+    });
+    console.log(temp);
+    setHerosList([...temp]);
+  }, [orderBy]);
+
   useState(() => {
-    const minimalList = createLimitedStatsList( listOfChampions);
+    const minimalList = createLimitedStatsList(listOfChampions);
     setHerosList(minimalList);
   }, []);
   return (
     <Box>
-      <Navbar/>
-      <Box>
-        <FormControl>
+      <Navbar />
+      <Box sx={{ display: "flex", justifyContent: "center", flexWrap: "wrap" }}>
+        <FormControl sx={{ paddingRight: "1rem" }}>
           <InputLabel id="heros_count">Count</InputLabel>
-          <Select labelId="heros_count" label="Count" value={heroCount} onChange={(event)=>handleCountChange(event) }>
+          <Select
+            className="heros_dropdown"
+            labelId="heros_count"
+            label="Count"
+            value={heroCount}
+            onChange={(event) => handleCountChange(event)}
+          >
             <MenuItem value={5}>5</MenuItem>
             <MenuItem value={10}>10</MenuItem>
             <MenuItem value={25}>25</MenuItem>
@@ -45,8 +74,26 @@ const Dashboard = () => {
             <MenuItem value={100}>100</MenuItem>
           </Select>
         </FormControl>
+        <FormControl>
+          <InputLabel id="heros_ordering">Order</InputLabel>
+          <Select
+            className="heros_dropdown"
+            labelId="heros_ordering"
+            label="Order By"
+            value={orderBy}
+            onChange={(event) => setOrderBy(event.target.value)}
+          >
+            <MenuItem value="hp">HP</MenuItem>
+            <MenuItem value="armor">Armor</MenuItem>
+            <MenuItem value="attackDamage">Attack damage</MenuItem>
+          </Select>
+        </FormControl>
       </Box>
-      <HerosList herosList={herosList} setHerosList={setHerosList} createLimitedStatsList={createLimitedStatsList} />
+      <HerosList
+        herosList={herosList}
+        setHerosList={setHerosList}
+        createLimitedStatsList={createLimitedStatsList}
+      />
     </Box>
   );
 };
