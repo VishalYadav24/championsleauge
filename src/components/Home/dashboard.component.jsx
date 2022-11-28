ccfa68c7e225956fc9e33a0611f105dcf5225d6e
VishalYadav24 <112162266+VishalYadav24@users.noreply.github.com>
2022-11-28 18:29:24 +0530

    addition - popover

--- src/components/Home/dashboard.component.jsx
+++ src/components/Home/dashboard.component.jsx
@@ -1,14 +1,25 @@
+import { Settings } from "@mui/icons-material";
 import { Box, FormControl, InputLabel, MenuItem, Select } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import { getListOfChampions } from "../../utils/api";
 import HerosList from "../Heros List/herosList.component";
 import Navbar from "../Navbar/navbar.component";
+import BasicPopover from "../popover/popover.component";
 import "./dashboard.styles.scss";
 /**
  * Container component for application components
  */
 const Dashboard = () => {
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const openPopover = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const closePopover = () => {
+    setAnchorEl(null);
+  };
   const [herosList, setHerosList] = useState([]);
   const [orderBy, setOrderBy] = useState("hp");
   const listOfChampions = useLoaderData();
@@ -57,7 +68,9 @@ const Dashboard = () => {
   return (
     <Box>
       <Navbar />
-      <Box sx={{ display: "flex", justifyContent: "center", flexWrap: "wrap" }}>
+      <Settings className="settings_icon" onClick={(event)=>openPopover(event)}/>
+      <BasicPopover anchorEl={anchorEl} openPopover={openPopover} closePopover={closePopover}>
+      <Box sx={{ display: "flex", justifyContent: "center", flexWrap: "wrap",padding:"1rem",background:"black" }}>
         <FormControl sx={{ paddingRight: "1rem" }}>
           <InputLabel id="heros_count">Count</InputLabel>
           <Select
@@ -89,6 +102,7 @@ const Dashboard = () => {
           </Select>
         </FormControl>
       </Box>
+      </BasicPopover>
       <HerosList
         herosList={herosList}
         setHerosList={setHerosList}
